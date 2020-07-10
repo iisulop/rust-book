cf7b0c6843936efc1d09cb3ff9a48331145f8d30
Iiro Sulopuisto <iisulop@gmail.com>
2020-07-10 08:58:10 +0300

    Chapter 17 Posts with different types

--- ch17-03/src/lib.rs
+++ ch17-03/src/lib.rs
@@ -1,120 +1,43 @@
-use std::cell::Cell;
-
-macro_rules! proxy_to_state {
-    ($($func_name:ident),+) => {
-        $(
-            pub fn $func_name(&mut self) {
-                if let Some(s) = self.state.take() {
-                    self.state = Some(s.$func_name())
-                }
-            }
-         )+
-    };
+pub struct Post {
+    content: String,
 }
 
-pub struct Post {
-    state: Option<Box<dyn State>>,
+pub struct DraftPost {
+    content: String,
+}
+
+pub struct PendingReviewPost {
     content: String,
 }
 
 impl Post {
-    pub fn new() -> Self {
-        Post {
-            state: Some(Box::new(Draft {})),
+    pub fn new() -> DraftPost {
+        DraftPost {
             content: String::new(),
         }
     }
 
-    pub fn add_text(&mut self, text: &str) -> Result<(), ()> {
-        if self.state.as_ref().unwrap().allow_modify() {
-            self.content.push_str(text);
-            Ok(())
-        } else {
-            Err(())
-        }
-    }
-
     pub fn content(&self) -> &str {
-        self.state.as_ref().unwrap().content(self)
-    }
-
-    proxy_to_state!(request_review, approve, reject);
-}
-
-trait State {
-    fn request_review(self: Box<Self>) -> Box<dyn State>;
-    fn approve(self: Box<Self>) -> Box<dyn State>;
-    fn content<'a>(&self, _post: &'a Post) -> &'a str {
-        ""
-    }
-    fn reject(self: Box<Self>) -> Box<dyn State>;
-    fn allow_modify(&self) -> bool {
-        false
+        &self.content
     }
 }
 
-struct Draft {}
-
-impl State for Draft {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        Box::new(PendingReview {
-            approval_count: Cell::new(0),
-        })
+impl DraftPost {
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
     }
 
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-
-    fn reject(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-
-    fn allow_modify(&self) -> bool {
-        true
-    }
-}
-
-struct PendingReview {
-    approval_count: Cell<usize>,
-}
-
-impl State for PendingReview {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        match self.approval_count.get() {
-            0 => {
-                self.approval_count.set(1);
-                self
-            }
-            _ => Box::new(Published {}),
+    pub fn request_review(self) -> PendingReviewPost {
+        PendingReviewPost {
+            content: self.content,
         }
     }
-
-    fn reject(self: Box<Self>) -> Box<dyn State> {
-        Box::new(Draft {})
-    }
 }
 
-struct Published {}
-
-impl State for Published {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-
-    fn content<'a>(&self, post: &'a Post) -> &'a str {
-        &post.content
-    }
-
-    fn reject(self: Box<Self>) -> Box<dyn State> {
-        self
+impl PendingReviewPost {
+    pub fn approve(self) -> Post {
+        Post {
+            content: self.content,
+        }
     }
 }
--- ch17-03/src/main.rs
+++ ch17-03/src/main.rs
@@ -3,29 +3,9 @@ use ch17_03::Post;
 fn main() {
     let mut post = Post::new();
 
-    assert_eq!(Ok(()), post.add_text("I ate a salad for lunch today"));
-    assert_eq!("", post.content());
-
-    post.request_review();
-    assert_eq!("", post.content());
-
-    assert_eq!(Err(()), post.add_text("\nNothing else"));
-
-    post.reject();
-    assert_eq!("", post.content());
-
-    assert_eq!(Ok(()), post.add_text("\nNothing else"));
-
-    post.request_review();
-    assert_eq!("", post.content());
-
-    post.approve();
-    assert_eq!("", post.content());
-    post.approve();
-    assert_eq!(Err(()), post.add_text("\nNothing else"));
-    assert_eq!(
-        "I ate a salad for lunch today\nNothing else",
-        post.content()
-    );
+    post.add_text("I ate a salad for lunch today");
+    let post = post.request_review();
+    let post = post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
     println!("{}", post.content())
 }
